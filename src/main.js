418c21f66bcf6ea3705fcb5e4f6cb42c85b7d4f8
meixuheng <824852963@qq.com>
2021-03-09 11:22:26 +0800

    完成了分类功能

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import './plugins/element.js'
 import './assets/css/global.css'
 // 导入字体图表
 import './assets/fonts/iconfont.css'
+// 导入TreeTable插件
+import TreeTable from 'vue-table-with-tree-grid'
 
 import axios from 'axios'
 // 配置请求的跟路径
@@ -18,6 +20,7 @@ Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
+Vue.component('tree-table', TreeTable)
 new Vue({
   router,
   render: h => h(App)
